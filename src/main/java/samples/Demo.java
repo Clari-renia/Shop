fd43936d2717cacb7fc981effc9c19c54500f604
Clari-renia <renataraczylo@gmail.com>
2021-07-24 10:56:46 +0200

    SampleClass add

---
+++ src/main/java/samples/Demo.java
@@ -0,0 +1,8 @@
+package samples;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        SampleClass sampleClass = new SampleClass("aaa", 1234);
+    }
+}
--- src/main/java/samples/SampleClass.java
+++ src/main/java/samples/SampleClass.java
@@ -1,4 +1,15 @@
 package samples;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+
 public class SampleClass {
+
+    private String firstField;
+    private int secondField;
 }
