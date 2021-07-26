2cbc8f12fb09749f58ff05013e4cf50988ff24d4
Clari-renia <renataraczylo@gmail.com>
2021-07-26 13:58:05 +0200

    SampleClass add

--- src/main/java/samples/Demo.java
+++ src/main/java/samples/Demo.java
@@ -4,5 +4,8 @@ public class Demo {
     public static void main(String[] args) {
 
         SampleClass sampleClass = new SampleClass("aaa", 1234);
+        sampleClass.setFirstField("qqq");
+        sampleClass.getFirstField();
+        SampleClass another = new SampleClass();
     }
 }
--- src/main/java/samples/Product.java
+++ src/main/java/samples/Product.java
@@ -1,4 +1,35 @@
 package samples;
 
 public class Product {
+    private String name;
+    private double weight;
+    private double price;
+
+    public Product( final String name, final double weight, final double price){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
